1041e5248c91d6cacff0cba650138bdbc9ed3bc0
Ryan-Matheus <ryanpqt266@gmail.com>
2024-08-07 23:49:12 -0300

    Commit #002 definindo api de autenticacao ao app + criando pasta de extensoes uteis

--- Spotify/Spotify/Managers/AuthManager.swift
+++ Spotify/Spotify/Managers/AuthManager.swift
@@ -10,7 +10,20 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "8ab75ab0b47a41c2ae99f865337d4007"
+        static let clientSecret = "1ebd950f7c6646b68716afde897e3500"
+    }
+    
     private init() {}
+    
+    public var singInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://github.com/Ryan-Matheus"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -30,5 +43,20 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(
+        code: String,
+        completion: @escaping ((Bool) -> Void)
+    ) {
+        
+    }
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
     }
 }
--- Spotify/Spotify/Resources/AppDelegate.swift
+++ Spotify/Spotify/Resources/AppDelegate.swift
@@ -25,6 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             window.rootViewController = navVC
         }
         window.makeKeyAndVisible()
+        
         return true
     }
 
